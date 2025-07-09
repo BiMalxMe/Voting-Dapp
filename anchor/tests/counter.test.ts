88a00c45da7460297e650252f57fe947d67ba6c9
BiMalxMe <journeyxbimal@gmail.com>
2025-07-09 14:39:12 +0545

    Fix : Contract Fix

--- anchor/tests/counter.test.ts
+++ anchor/tests/counter.test.ts
@@ -3,24 +3,29 @@ import { Program } from '@coral-xyz/anchor'
 import { BankrunProvider, startAnchor } from "anchor-bankrun";
 import { Keypair, PublicKey } from '@solana/web3.js'
 import {Voting} from "../target/types/voting"
-import { Anchor } from 'lucide-react';
 import { expect,it, beforeAll } from '@jest/globals'; // <--- Correct import for Jest globals
 import { describe } from 'node:test';
 const IDL = require("../target/idl/voting.json")
 
 const votingAddress  = new PublicKey("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS")
+describe("voting",()=>{
+  let context ;
+  let provider ;
+  let votingProgram: anchor.Program<Voting>;
 
+  beforeAll(async()=>{
+     context = await startAnchor("",[{name : "voting",programId : votingAddress}],[]);
+     provider = new BankrunProvider(context);
+  
+     votingProgram = new Program<Voting>(IDL,provider)
+
+  })
 it('counter', async() => {
-  const context = await startAnchor("",[{name : "voting",programId : votingAddress}],[]);
-  const provider = new BankrunProvider(context);
-
-  const votingProgram = new Program<Voting>(IDL,provider)
-
   await votingProgram.methods.initializePoll(
       new anchor.BN(1),
       "What is your favourite phone brand",
       new anchor.BN(Date.now()/1000),
-      new anchor.BN(1852039640/1000 + 3600),
+      new anchor.BN(Date.now()/1000 + 3600),
   ).rpc()
 
     const [pollAddress] = PublicKey.findProgramAddressSync(
@@ -30,9 +35,32 @@ it('counter', async() => {
       const poll = await votingProgram.account.poll.fetch(pollAddress);
       console.log(poll)
 
-      // Use Jest's expect for this assertion
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
       expect(poll.pollId.toNumber()).toBe(1);
       expect(poll.description).toBe("What is your favourite phone brand");
-      expect(poll.pollStart.toNumber()).toBeGreaterThan(poll.pollEnd.toNumber());
+      expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
   })
+
+  it('initialize candidates', async () => {
+    const [androidid] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer,"le",8),Buffer.from("Android")],
+      votingAddress
+    )
+    const [appleid] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer,"le",8),Buffer.from("Apple")],
+      votingAddress
+    )
+    const [pollpda] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer,"le",8)],
+      votingAddress
+    )
+    await votingProgram.methods.initializeCandidate(
+      "Apple", 
+      new anchor.BN(1),
+    ).accounts({
+        
+    })
+  });
+  it("vote",async() => {
+    
+  })
+})
